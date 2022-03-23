c910263941e4b264b8c3fc99bdd17d7e8a4f374e
Jigar1992 <jatin.parmar@tatvasoft.com>
2022-03-23 18:44:57 +0530

    Push

--- WebAPI/WebAPI/Startup.cs
+++ WebAPI/WebAPI/Startup.cs
@@ -75,8 +75,10 @@ namespace WebAPI
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ApplicationDbContext appDbContext)
         {
+            appDbContext.Database.Migrate();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -109,6 +111,8 @@ namespace WebAPI
                     pattern: "{controller=Home}/{action=Index}/{id?}");
                 endpoints.MapRazorPages();
             });
+
+            
         }
     }
 }
