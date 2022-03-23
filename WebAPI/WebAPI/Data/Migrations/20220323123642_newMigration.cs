6e891b6c96898b0751a7beac0352f1f3b2f62e83
Jigar1992 <jatin.parmar@tatvasoft.com>
2022-03-23 18:29:33 +0530

    Push all changes

---
+++ WebAPI/WebAPI/Data/Migrations/20220323123642_newMigration.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebAPI.Data.Migrations
+{
+    public partial class newMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Manufacturer",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    manufacturer_name = table.Column<string>(maxLength: 50, nullable: false),
+                    country = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Manufacturer", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "VehicleStatus",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    status_name = table.Column<string>(maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_VehicleStatus", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Model",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Manufacturer_Id = table.Column<int>(nullable: false),
+                    Model_name = table.Column<string>(maxLength: 50, nullable: false),
+                    first_production_date = table.Column<DateTime>(type: "datetime", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Model", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Model_Manufacturer",
+                        column: x => x.Manufacturer_Id,
+                        principalTable: "Manufacturer",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Vehicles",
+                columns: table => new
+                {
+                    id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    status_id = table.Column<int>(nullable: false),
+                    model_id = table.Column<int>(nullable: true),
+                    year = table.Column<int>(nullable: true),
+                    colour = table.Column<string>(maxLength: 50, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Vehicles", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_Vehicles_Model",
+                        column: x => x.model_id,
+                        principalTable: "Model",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Vehicles_VehicleStatus",
+                        column: x => x.status_id,
+                        principalTable: "VehicleStatus",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Model_Manufacturer_Id",
+                table: "Model",
+                column: "Manufacturer_Id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Vehicles_model_id",
+                table: "Vehicles",
+                column: "model_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Vehicles_status_id",
+                table: "Vehicles",
+                column: "status_id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Vehicles");
+
+            migrationBuilder.DropTable(
+                name: "Model");
+
+            migrationBuilder.DropTable(
+                name: "VehicleStatus");
+
+            migrationBuilder.DropTable(
+                name: "Manufacturer");
+        }
+    }
+}
